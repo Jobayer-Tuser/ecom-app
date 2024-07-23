9c24a080d3e862171f928a336b16f0496706e7e2
Jobayer Al Madmud Tuser <jtuser112@gmail.com>
2024-07-23 12:42:13 +0600

    created ProductVariants controller, model, factories, request, seeder

---
+++ modules/Product/Database/factories/ProductVariantFactory.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Modules\Product\Database\factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\Modules\Product\Models\ProductVariant>
+ */
+class ProductVariantFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            //
+        ];
+    }
+}
---
+++ modules/Product/Database/seeders/ProductVariantSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Modules\Product\Database\seeders;
+
+use Illuminate\Database\Seeder;
+
+class ProductVariantSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+    }
+}
